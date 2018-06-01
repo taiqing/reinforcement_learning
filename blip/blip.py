0d8f877b54ec3a2b5078ea68bef90c676d89a981
taiqing <taiqing@amazon.com>
2018-05-31 18:52:04 -0700

    blip.py: calculate and print ctr

--- blip/blip.py
+++ blip/blip.py
@@ -6,6 +6,7 @@ Bayesian Linear Probit Model
 
 from scipy import stats
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sample_norm_distr(mean, var, num):
@@ -33,33 +34,34 @@ def simulate_reward(true_w, beta, x):
 
 
 def f_v(t):
-    return stats.norm.pdf(t) / (stats.norm.cdf(t) + 1e-6)
+    return stats.norm.pdf(t) / stats.norm.cdf(t)
 
 
 def f_w(t):
     return f_v(t) * (f_v(t) + t)
 
 
-def main():
-# if __name__ == '__main__':
+# def main():
+if __name__ == '__main__':
     x_prior = 0.2
     # dimension
     d_x = 5
     # number of samples
-    n_x = 1000
-    true_w = np.array([-1, 1, -1, 1, -1], dtype=np.float32)
+    n_x = 2000
+    true_w = np.array([-1, -1, -1, 1, 1], dtype=np.float32)
     w_init_mean = np.array([0] * d_x, dtype=np.float32)
-    w_init_var = np.array([10] * d_x, dtype=np.float32)
-    n_iter = 10000
-    beta = 10.
+    w_init_var = np.array([10.] * d_x, dtype=np.float32)
+    n_iter = 1000
+    beta = 1.
     probe_steps = 100
 
     # generate binary samples: 2d np.array
     X = stats.bernoulli.rvs(x_prior, size=[n_x, d_x])
     print 'note: {a} should be close to {p}'.format(p=x_prior, a=1.0 * X.sum() / X.size)
-    
+
     w_mean = w_init_mean
     w_var = w_init_var
+    reward_list = []
     for i in xrange(n_iter):
         # sample w
         w = sample_norm_distr(w_mean, w_var, 1)[0]
@@ -69,6 +71,7 @@ def main():
         x = X[np.argmax(p)]
         # receive the reward
         y = simulate_reward(true_w, beta, x)
+        reward_list.append(y)
         y = 2 * y - 1
         if i % probe_steps == 0:
             print '{}-th iter'.format(i)
@@ -84,7 +87,13 @@ def main():
             print 'w_var: {a} -> {b}'.format(a=w_var, b=w_var_updated)
         w_mean = w_mean_updated
         w_var = w_var_updated
+    ctr = np.convolve(reward_list, np.ones(50, np.float32) / 50, mode='valid')
+    fig = plt.figure(0)
+    ax = fig.add_subplot(111)
+    ax.plot(ctr)
+    fig.show()
+    
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
